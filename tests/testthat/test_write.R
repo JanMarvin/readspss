4a23aaced08feefd16a8790d22706385be767543
Jan Marvin Garbuszus <jan.garbuszus@ruhr-uni-bochum.de>
2018-06-24 16:12:40 +0200

    write test for str

--- tests/testthat/test_write.R
+++ tests/testthat/test_write.R
@@ -1,4 +1,4 @@
-
+#### test 1 ####
 if (dir.exists("data"))
   unlink("data", recursive = TRUE)
 
@@ -14,5 +14,23 @@ dd <- read.sav("data/cars.sav")
 test_that("write test", {
     expect_true(all.equal(cars, dd, check.attributes = FALSE))
     })
+
+unlink("data", recursive = TRUE)
+
+#### test 2 ####
+if (dir.exists("data"))
+  unlink("data", recursive = TRUE)
+
+dir.create("data")
+
+df <- data.frame(V1 = letters, V2 = 1:26, stringsAsFactors = FALSE)
+# can not really write anything else
+write.sav(df, filepath = "data/df.sav")
+
+dd <- read.sav("data/df.sav")
+
+test_that("write test2", {
+  expect_true(all.equal(dd, df, check.attributes = FALSE))
+})
 
 unlink("data", recursive = TRUE)
