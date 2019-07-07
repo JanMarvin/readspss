6db9ee5d839908fbd352bc1f4aa430975ea27aed
Jan Marvin Garbuszus <jan.garbuszus@ruhr-uni-bochum.de>
2019-07-07 16:54:11 +0200

    move data part to write_data()

---
+++ src/write_data.h
@@ -0,0 +1,26 @@
+/*
+ * Copyright (C) 2019 Jan Marvin Garbuszus
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation; either version 2 of the License, or (at your
+ * option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef WRITE_DATA_H
+#define WRITE_DATA_H
+
+void write_data(Rcpp::DataFrame dat, bool cflag,
+                int64_t n, int32_t kk,
+                Rcpp::IntegerVector vtyp, Rcpp::IntegerVector itc,
+                Rcpp::IntegerVector cc, std::fstream& sav, bool swapit);
+
+#endif
