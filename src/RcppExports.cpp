ada5e142aca947fa9b5f7a5750e316efc98856c6
Jan Marvin Garbuszus <jan.garbuszus@ruhr-uni-bochum.de>
2021-10-30 12:08:47 +0200

    update RcppExports.cpp

--- src/RcppExports.cpp
+++ src/RcppExports.cpp
@@ -5,6 +5,11 @@
 
 using namespace Rcpp;
 
+#ifdef RCPP_USE_GLOBAL_ROSTREAM
+Rcpp::Rostream<true>&  Rcpp::Rcout = Rcpp::Rcpp_cout_get();
+Rcpp::Rostream<false>& Rcpp::Rcerr = Rcpp::Rcpp_cerr_get();
+#endif
+
 // boost_split
 Rcpp::CharacterVector boost_split(std::string val_s);
 RcppExport SEXP _readspss_boost_split(SEXP val_sSEXP) {
